325225cc82848fa9a519e4e4b2a5179ae474d483
Octasanchezz <101231228+Octasanchezz@users.noreply.github.com>
2022-12-06 18:24:00 -0300

    Commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h3>Bienvenidos a la APP</h3>
+    </div>
+  );
+}
+
+export default App;
